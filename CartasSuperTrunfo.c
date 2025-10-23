b564ea103213e0cb12a7d044ada48d1b20da8117
natanznx0-commits <natanznx0@gmail.com>
2025-10-23 00:00:03 +0000

    conclusão

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,37 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-printf("=== Desafio Super Trunfo - Países ===\n ");
+   int população, pontosTuristicos;
+   float area;
+   float pib;
+   char codigo [4];
+  
+
   // Área para entrada de dados
+
+  printf("Digite o Codigo da Carta:");
+  scanf("%s", &codigo);
+
+  printf("Digite a população:");
+  scanf("%d", &população);
+
+  printf("Digite Pontos turisticos:");
+  scanf("%d", &pontosTuristicos);
+
+  printf("Digite a Área:");
+  scanf("%f", &area);
+
+  printf("Digite o Pib:");
+  scanf("%f", &pib);
 
   // Área para exibição dos dados da cidade
 
+ printf("--- Dados de Cartas ---\n");
+
+  printf("Codigo da Carta:%s\n", codigo);
+  printf("População:%d\n", população);
+  printf("Pontos turisticos:%d\n", pontosTuristicos);
+  printf("Área:%f\n", area);
+  printf("Pib:%f\n", pib);
 return 0;
 } 
